920152c7088cfd1b73dae705af8912d99b032f27
Shentuo Zhan <shentuo.zhan@tigerspike.com>
2017-06-14 10:02:25 +0800

    added GCE module

--- app/src/main/java/com/shentuo/builditbigger/MainActivity.java
+++ app/src/main/java/com/shentuo/builditbigger/MainActivity.java
@@ -1,7 +1,9 @@
 package com.shentuo.builditbigger;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -40,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void tellJoke(View view) {
         Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Testing from BuildItBigger"));
     }
 
 
