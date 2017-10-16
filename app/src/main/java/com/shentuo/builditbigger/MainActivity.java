8fba1370e4b6bebc5fc80f98a309263f9397c7b9
Shentuo Zhan <shentuo.zhan@tigerspike.com>
2017-10-16 11:00:58 +0800

    Revert "added GCE module"
    This reverts commit 920152c7088cfd1b73dae705af8912d99b032f27.

--- app/src/main/java/com/shentuo/builditbigger/MainActivity.java
+++ app/src/main/java/com/shentuo/builditbigger/MainActivity.java
@@ -1,9 +1,7 @@
 package com.shentuo.builditbigger;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -42,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void tellJoke(View view) {
         Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
-        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Testing from BuildItBigger"));
     }
 
 
