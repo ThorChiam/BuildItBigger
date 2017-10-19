72faf583cbf8333c45b630c810e58ac542445e47
Shentuo Zhan <shentuo.zhan@tigerspike.com>
2017-10-19 23:16:16 +0800

    Add functional test

--- app/src/androidTest/java/com/shentuo/builditbigger/RetrieveJokesTest.java
+++ app/src/androidTest/java/com/shentuo/builditbigger/RetrieveJokesTest.java
@@ -8,6 +8,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
+
 /**
  * Created by ShentuoZhan on 16/10/17.
  */
@@ -24,6 +32,13 @@ public class RetrieveJokesTest {
 
     @Test
     public void retrieveJokes() {
-        new EndpointsAsyncTask().execute(mActivityRule.getActivity());
+        // Press the button.
+        onView(withId(R.id.tell_joke_btn)).perform(click());
+
+        // Check that the text not empty or error message.
+        onView(withId(R.id.jokeContents)).check(matches(notNullValue()));
+        onView(withId(R.id.jokeContents)).check(matches(not(withText(""))));
+        onView(withId(R.id.jokeContents))
+                .check(matches(not(withText(mActivityRule.getActivity().getString(R.string.error_message)))));
     }
 }
